7691650bcd9582d5af1ad30611f1b9a803eaadf8
Abdul Moeed <amoeed78@gmail.com>
2020-06-01 11:36:53 -0400

    Add new app deploy project

---
+++ iims_eap_prod/roles/deployment/templates/shutdown-iims.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /apps/devjboss/.bash_profile 
+sh -x /apps/devjboss/jboss-eap-7.1/shutdown-iims.sh
+
+
+
---
+++ iims_eap_prod/roles/deployment/templates/start-iims.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /apps/devjboss/.bash_profile 
+sh -x /apps/devjboss/jboss-eap-7.1/start-iims.sh
+echo "eap is started with pid"
+#!/bin/bash
+
+
+
+
